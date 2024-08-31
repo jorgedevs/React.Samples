80adab0cd933bc9ba7af27a02b2074cc5bc72fa5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-08-30 22:09:42 -0700

    Add use-effect-hook sample

---
+++ Source/use-effect-hook-welcome/src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  const [toggle, setToggle] = React.useState(false);
+
+  const clickHandler = () => {
+    setToggle(!toggle);
+  }
+
+  React.useEffect(()=> {
+    document.title = toggle
+    ? "Welcome to Little Lemon"
+    : "Using the useEffect hook"
+  },[toggle]);
+  //},[]); // Run only once...
+
+  return (
+    <div>
+      <h1>Using the useEffect hook</h1>
+      <button onClick={clickHandler}>
+        Toggle message
+      </button>
+      {toggle &&  <h2>Welcome to Little Lemon</h2>}
+    </div>
+  );
+}
+
+export default App;
